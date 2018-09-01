459029207db72c98dbb074b16abdbda0ce779471
JoshuaBatty <joshpbatty@gmail.com>
2018-09-01 11:34:03 +0700

    making the trade and book module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,32 @@
-#![deny(
-    unstable_features,
-    unused_must_use,
-    unused_mut,
-    unused_imports,
-    unused_import_braces)]
+#![deny(unstable_features, unused_must_use, unused_mut, unused_imports, unused_import_braces)]
 
-#[macro_use] 
+#[macro_use]
 extern crate error_chain;
 
 extern crate hex;
-extern crate ring;
 extern crate reqwest;
+extern crate ring;
 extern crate serde;
 #[macro_use]
 extern crate serde_json;
 extern crate tungstenite;
 extern crate url;
 
-#[macro_use] 
+#[macro_use]
 extern crate serde_derive;
 
-mod book;
-mod client;
-mod ticker;
-mod trades;
-mod orders;
-mod candles;
 mod account;
+pub mod book;
+mod candles;
+mod client;
+mod orders;
+pub mod ticker;
+mod trades;
 
 pub mod api;
-pub mod pairs;
 pub mod currency;
+pub mod errors;
+pub mod events;
+pub mod pairs;
 pub mod precision;
 pub mod websockets;
-pub mod events;
-pub mod errors;
